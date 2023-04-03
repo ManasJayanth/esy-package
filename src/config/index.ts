1090f625988f1905ad78f7f6153238b364981bc6
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-04-03 10:58:13 +0530

    $HOME alternative for Windows

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export let localNpmRc = `${process.env.HOME}/.npmrc`; // TODO centralise this
+export let localNpmRc = `${process.platform === 'win32' ? process.env.USERPROFILE: process.env.HOME}/.npmrc`; // TODO centralise this
 export let REGISTRY_ADDR = "localhost";
 export let REGISTRY_PORT = 6000;
 export let REGISTRY_HOST = `${REGISTRY_ADDR}:${REGISTRY_PORT}`;
