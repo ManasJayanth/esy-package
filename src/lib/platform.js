58a39863b211c49a82918584ace4500582bebb59
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-10-08 15:45:10 +0530

    Add utils

---
+++ src/lib/platform.js
@@ -0,0 +1,76 @@
+const cp = require("child_process");
+
+let isWindows;
+let isMacos;
+let isLinux;
+let isIntel;
+let isArm64;
+let isCygwin;
+let pathSep;
+
+function uname() {
+  return new Promise(function (resolve, reject) {
+    let { spawn } = cp;
+    let uname;
+    uname = spawn("uname", ["-a"]);
+    let stdout = "",
+      stderr = "";
+
+    uname.on("error", (err) => {
+      reject(err);
+    });
+
+    uname.stdout.on("data", (data) => {
+      stdout += data;
+    });
+
+    uname.stderr.on("data", (data) => {
+      stderr += data;
+    });
+
+    uname.on("close", (code) => {
+      if (code === 0) {
+        resolve(stdout);
+      } else {
+        reject(
+          new Error(`uname failed to run:
+exit code: ${code}
+stderr: ${stderr}`)
+        );
+      }
+    });
+  });
+}
+
+async function scan() {
+  try {
+    let unameStr = await uname();
+    isCygwin = /cygwin/.test(unameStr);
+  } catch (e) {
+    isCygwin = false;
+  }
+
+  isWindows = process.platform == "win32";
+  isMacos = process.platform == "darwin";
+  isLinux = process.platform == "linux";
+  isIntel = process.arch == "x64";
+  isArm64 = process.arch == "arm64";
+
+  if (isWindows && !isCygwin) {
+    pathSep = ";";
+  } else {
+    pathSep = ":";
+  }
+
+  return {
+    isWindows,
+    isMacos,
+    isLinux,
+    isIntel,
+    isArm64,
+    isCygwin,
+    pathSep,
+  };
+}
+
+module.exports.scan = scan;
---
+++ src/lib/resolve-cmd.js
@@ -0,0 +1,49 @@
+const fs = require("fs-extra");
+const path = require("path");
+const debugF = require("debug");
+
+export async function resolveCmd(platform, cmd) {
+  let debug = debugF("resolveCmd");
+  let envPath = process.env["PATH"];
+  let paths = envPath.split(platform.pathSep);
+  let possibleCmdExtensions;
+  if (platform.isWindows) {
+    possibleCmdExtensions = [".exe", ".ps1", ".cmd"];
+    if (platform.isCygwin) {
+      possibleCmdExtensions.push("");
+    }
+  } else {
+    possibleCmdExtensions = [""];
+  }
+
+  let possibleFilePaths = paths.reduce((acc, p) => {
+    return acc.concat(
+      possibleCmdExtensions.map((ext) => path.join(p, `${cmd}${ext}`))
+    );
+  }, []);
+
+  let result = null;
+
+  for (possibleFilePath of possibleFilePaths) {
+    if (!!result) {
+      debug("Found at", result);
+      break;
+    }
+    let fullPath = possibleFilePath;
+    try {
+      debug("Trying %s", fullPath);
+      let stats = await fs.lstat(fullPath);
+      if (stats.isSymbolicLink()) {
+        result = await fs.realpath(fullPath);
+      } else if (stats.isFile()) {
+        result = fullPath;
+      } else return null;
+    } catch (e) {
+      if (e.errno === -2 && e.code === "ENOENT") {
+        debug("Doesn't exist", e.path);
+      }
+    }
+  }
+
+  return result;
+}
