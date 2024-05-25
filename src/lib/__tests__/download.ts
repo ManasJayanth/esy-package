e18021c085d8486974ad5d9f311a03eb3518fc59
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-05-25 15:59:08 +0530

    e2e test: fixes outdated hash

--- src/lib/__tests__/download.ts
+++ src/lib/__tests__/download.ts
@@ -5,10 +5,10 @@ const fs = require("fs");
 
 test("Must download a file correctly", async function () {
   let tmp = genTemp("download-success-test");
-  let indexHTMLPath = path.join(tmp, "index.html");
+  let tarballPath = path.join(tmp, "tarball");
   await download(
-    "https://esy.sh#sha1:11f4a06342b45dbf9656cb7c2c11a48ee1055c6e",
-    indexHTMLPath,
+    "https://github.com/esy/test-hello-c/archive/refs/tags/0.1.0.tar.gz#b347c1eb3db9a55ad9cee916d5ca16a104843b45",
+    tarballPath,
   );
-  expect(fs.existsSync(indexHTMLPath)).toBe(true);
+  expect(fs.existsSync(tarballPath)).toBe(true);
 });
