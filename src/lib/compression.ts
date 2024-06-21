3dbda341b08ae228da805fb44c883de104ed9606
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-06-21 13:13:37 +0530

    Adds support for Powershell on Windows

--- src/lib/compression.ts
+++ src/lib/compression.ts
@@ -1,18 +1,34 @@
 import * as cp from "child_process";
+import * as fs from "fs";
 import * as path from "path";
 import { cygpath } from "./utils";
 import Debug from "debug";
+import promisepipe from "promisepipe";
+import tarFs from "tar-fs";
+import gunzipMaybe from "gunzip-maybe";
 
 const debug = Debug("bale:compression");
+
+async function extractTarball(filePath: string, destDir: string) {
+  await promisepipe(
+    fs.createReadStream(filePath),
+    gunzipMaybe(),
+    tarFs.extract(destDir),
+  );
+}
 
 export async function tar(filePath, destDir, gzip?) {
   filePath = await cygpath(filePath);
   destDir = await cygpath(destDir);
-  const cmd = `tar -x${gzip ? "z" : ""}f ${filePath} -C ${destDir}`;
-  debug("Running", cmd);
-  cp.execSync(cmd, {
-    stdio: "inherit",
-  });
+  if (process.platform === "win32") {
+    await extractTarball(filePath, destDir);
+  } else {
+    const cmd = `tar -x${gzip ? "z" : ""}f ${filePath} -C ${destDir}`;
+    debug("Running", cmd);
+    cp.execSync(cmd, {
+      stdio: "inherit",
+    });
+  }
 }
 
 export async function unzip(filePath, destDir) {
